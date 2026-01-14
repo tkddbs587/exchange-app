93bd806d27dbc45f19de240529dc85b3726e401e
hoon <dudrn587@gmail.com>
2026-01-14 16:19:32 +0900

    feat: 인증 관리 zustand 스토어 추가

---
+++ src/store/authStore.ts
@@ -0,0 +1,30 @@
+import { create } from 'zustand';
+import { TOKEN_KEY } from '../constants/auth';
+
+interface AuthState {
+  token: string | null;
+  isHydrated: boolean;
+  hydrate: () => void;
+  setToken: (token: string) => void;
+  clearToken: () => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  token: null,
+  isHydrated: false,
+
+  hydrate: () => {
+    const token = localStorage.getItem(TOKEN_KEY);
+    set({ token, isHydrated: true });
+  },
+
+  setToken: (token) => {
+    localStorage.setItem(TOKEN_KEY, token);
+    set({ token });
+  },
+
+  clearToken: () => {
+    localStorage.removeItem(TOKEN_KEY);
+    set({ token: null });
+  },
+}));
