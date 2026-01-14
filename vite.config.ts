05d85329f54c102b5d8891da2c20bee380f37fbd
hoon <dudrn587@gmail.com>
2026-01-14 15:04:00 +0900

    feat: api 프록시 설정

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://exchange-example.switchflow.biz',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
