abaea6672ad04392b67303a335f777fe01765753
hoon <dudrn587@gmail.com>
2026-01-14 21:08:28 +0900

    feat: 환전 페이지 헤더 UI 추가

---
+++ src/pages/exchange-money/_components/ExchangeHeader/ExchangeHeader.tsx
@@ -0,0 +1,12 @@
+export function ExchangeHeader() {
+  return (
+    <div className="flex flex-col gap-2">
+      <h1 className="text-[40px] font-bold leading-[53px] text-[#28323C]">
+        환율 정보
+      </h1>
+      <h2 className="text-[20px] leading-[27px] text-[#374553]">
+        실시간 환율을 확인하고 간편하게 환전하세요.
+      </h2>
+    </div>
+  );
+}
