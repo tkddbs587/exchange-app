c55fe1644ebf4354c6aabdaa93db02dde603e3b2
hoon <dudrn587@gmail.com>
2026-01-14 15:03:32 +0900

    feat: query provider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Router } from './routes/Router';
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
     <BrowserRouter>
-      <Router />
+      <QueryClientProvider client={queryClient}>
+        <Router />
+      </QueryClientProvider>
     </BrowserRouter>
   );
 }
